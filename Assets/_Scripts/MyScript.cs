4515d1ffaa3ccea7af979a51e2a49b98d2810093
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-09-16 10:55:47 -0700

    Added logic

--- Assets/_Scripts/MyScript.cs
+++ Assets/_Scripts/MyScript.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class MyScript : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float forceValue;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(new Vector3(0, forceValue, 0));
     }
 }
